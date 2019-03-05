7f23b7ae22ea3ac092ee3b3b2f162fa6ffc4dc31
minjae <32402820+kkrlee@users.noreply.github.com>
2019-03-05 15:13:02 -0500

    first upload

---
+++ codeit_flip.py
@@ -0,0 +1,12 @@
+def flip(some_list):
+
+    if len(some_list) < 2:
+        return some_list
+    return flip(some_list[1:]) + some_list[:1]
+
+
+
+
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = flip(some_list)
+print(some_list)
